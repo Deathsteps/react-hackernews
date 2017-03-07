cedfed456b6bf59c021f662e1ee6790fc674ec75
Deathsteps <lovely_dreamer@126.com>
2017-03-07 20:36:16 +0800

    HN api

---
+++ src/store/api.js
@@ -0,0 +1,40 @@
+import Firebase from 'firebase/app'
+import 'firebase/database'
+
+const config = {
+  databaseURL: 'https://hacker-news.firebaseio.com'
+}
+const version = '/v0'
+
+Firebase.initializeApp(config)
+const api = Firebase.database().ref(version)
+
+function fetchData(path) {
+  return new Promise((resolve, reject) => {
+    api.child(path).once('value', snapshot => {
+      const val = snapshot.val()
+      resolve(val)
+    }, reject)
+  })
+}
+
+function fetchItem(id) {
+  return fetchData('item/' + id)
+}
+
+function fetchItems(ids) {
+  return Promise.all(ids.map(id => fetchItem(id)))
+}
+
+export const getTopStories = () => {
+  return new Promise(function(resolve, reject) {
+    fetchData('topstories').then(
+      ids => fetchItems(ids.slice(0, 30)).then(resolve, reject),
+      reject
+    )
+  });
+}
+
+export const getStoryComments = (commentIds) => {
+  return fetchItems(commentIds)
+}
