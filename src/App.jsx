bedaf6db32fc11a2ab225d33e28f2632d7fcede7
Deathsteps <lovely_dreamer@126.com>
2017-03-08 13:53:59 +0800

    StoryList Style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import './assets/App.css'
+import './assets/Story.css'
 
 class App extends Component {
   componentDidMount () {
@@ -9,13 +9,31 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <header className="App-header">
+          <h1>Hacker News</h1>
+        </header>
+        <div className="Story-list">
+        {
+          [1,2,3,4,5,6,7].map(() => {
+            return (
+              <div className="Story-item">
+                <h3 className="Story-title">
+                  <a href="#">Finding Free Food with Python</a>
+                </h3>
+                <div className="Story-source">
+                  <a href="#">jamesbvaughan.com</a>
+                </div>
+                <div className="Story-info">
+                  116 points by <a href="#">jamesbvaughan</a> 4 hours ago
+                </div>
+                <div className="Story-comments">
+                  <a href="#">15 comments</a>
+                </div>
+              </div>
+            )
+          })
+        }
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     )
   }
